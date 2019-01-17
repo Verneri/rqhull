5958420474235981da64fce36153ff4ebb633403
Verneri Åberg <verneri.aberg@gmail.com>
2019-01-17 10:35:07 +0200

    checkout correct version

--- build.rs
+++ build.rs
@@ -2,10 +2,23 @@ use git2::{Repository, Error};
 
 
 fn main() {
-    clone_qhull("./qhull");
+    let repo = clone_qhull("./qhull").expect("failed to clone qhull");
+    checkout_version(repo, "v7.3.0").expect("checkout version failed");
+    
 }
 
 fn clone_qhull(qhull_dir: &str) -> Result<Repository, Error> {
     let url = "https://github.com/qhull/qhull.git";
     Repository::clone(url, qhull_dir)
+}
+
+fn checkout_version(repo : Repository, revision: &str) -> Result<(), Error> {
+    use git2::build::CheckoutBuilder;
+
+    let mut build = CheckoutBuilder::new();
+    let rev = repo.revparse_single(revision)?;
+
+    repo.checkout_tree(&rev,
+                       Some(build
+                           .force()))
 }
